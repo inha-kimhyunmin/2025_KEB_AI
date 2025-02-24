a084fad78ec228689f3122fc388f6ded16016593
inha-kimhyunmin <kimhm396@gmail.com>
2025-02-24 20:09:00 +0900

    v0.7 day1homework

--- KNeighborsRegressor.py
+++ KNeighborsRegressor.py
@@ -7,6 +7,8 @@ class KNeighborsRegressor:
         self.n_neighbors = n_neighbors
         self.nearest_neighbors = None
         self.sorted_indices = None
+        self.x = None
+        self.y = None
 
     def fit(self, x_new, x, y):
         """
@@ -15,9 +17,17 @@ class KNeighborsRegressor:
         :param y: dependent variable list(y-axis)
         :return:
         """
+        self.x = x
+        self.y = y
         distance = np.abs(x - x_new)
         self.sorted_indices = np.argsort(distance, axis=0)
         self.nearest_neighbors = y[self.sorted_indices[:self.n_neighbors]]
 
     def predict(self):
         return np.mean(self.nearest_neighbors)
+
+    def predict_by_repetition(self):
+        sum = 0
+        for i in range(self.n_neighbors):
+            sum += self.y[self.sorted_indices[i][0]][0]
+        print(sum / 5)
